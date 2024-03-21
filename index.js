df63a39859eee31e11fd498b868b67d546efd08b
Elvis Moses Tangasaiti <elvis.moses@student.moringaschool.com>
2024-03-21 11:46:08 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your solution in this file!
+var customerName='bob';
+function  upperCaseCustomerName(){ return customerName = customerName.toUpperCase();}
+function setBestCustomer(){return bestCustomer='not bob';}
+function overwriteBestCustomer(){return bestCustomer='maybe bob';}
+const leastFavoriteCustomer=`james`
+function changeLeastFavoriteCustomer(){return leastFavoriteCustomer=`ted`;}
